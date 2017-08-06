24fcc0be120202f386c45ea855680f9c0bf97fd2
Nick Nance <nicknance@Nicks-MBP-2.lan>
2017-08-05 21:02:46 -0700

    Guest search working

--- public/rsvp/main.js
+++ public/rsvp/main.js
@@ -4,19 +4,35 @@ const rsvpState = {
 };
 
 // INITIAL FETCH
-$.get('api/guests', function(data) {
-  rsvpState.guestList = data;
-})
+$.get('/api/guests', function(data) {
+  const objData = JSON.parse(data);
+  setGuestList(objData.guests);
+});
+
+// HELPERS
+function buildListDom(list) {
+  const searchVal = $('#name-field').val();
+  const guestList = list.filter(function(guest) {
+    return guest.guestName.toLowerCase().indexOf(searchVal.toLowerCase()) !== -1;
+  });
+  return guestList.map(function(guest) {
+    return '<li data-id="' + guest.id + '"">' + guest.guestDisplayName + '</li>';
+  });
+}
+
+// ACTIONS *CHANG STATE*
+function setGuestList(list) {
+  rsvpState.guestList = list;
+}
 
 $(document).ready(function() {
-  $('#name-button').on('click', function() {
-    const searchVal = $('#name-field').val();
-    rsvpState.guestList.filter(function(guest) {
-      return guest.guestName.indexOf(searchVal) !== -1;
-    });
-    const guestListDom = rsvpState.guestList.map(function(guest) {
-      return '<li>' + guest.guestName + '</li>';
-    })
-    $('#guest-list').html(guestListDom.join());
+
+  $('#name-form').on('submit', function(e) {
+    e.preventDefault();
+    $('#guest-list').html(buildListDom(rsvpState.guestList));
+  });
+
+  $('#guest-list').on('click', function(e) {
+    console.log("TARGET", e);
   });
 });
