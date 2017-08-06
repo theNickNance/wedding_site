272b988d24ba428280ea687699bd8c47bfd045af
Nick Nance <nick.nance@creditkarma.com>
2017-08-05 21:06:01 -0700

    parse put body

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import * as express from 'express'
 import * as config from 'config'
+import * as bodyParser from 'body-parser'
 import * as fs from 'fs'
+
 import routes from './routes'
 
 const app = express()
 const port = config.get('expressPort')
+
+app.use(bodyParser.json())
 
 app.use('/', express.static('public'))
 app.use('/api', routes)
--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,13 @@
 import * as express from 'express'
 import * as fs from 'fs'
 
-import { IGuest } from './types'
+import { IGuest, IGuestUpdate } from './types'
 
 const router = express.Router()
 
 router.get('/guests', (req, res) => {
-    let guests: Array<IGuest>
-
     fs.readFile('./fixtures/guests.json', (err, buffer) => {
-        guests = JSON.parse(buffer.toString('utf8'))
+        const guests = JSON.parse(buffer.toString('utf8')) as Array<IGuest>
         res.setHeader('Content-Type', 'application/json');
         res.json(guests)
     })
@@ -17,7 +15,9 @@ router.get('/guests', (req, res) => {
 
 router.put('/guests/:_id', (req, res) => {
     const id = req.params._id
-    console.log(`got id ${id}`)
+    const guestUpdate = req.body as IGuestUpdate
+
+    console.log(`got id ${id} count ${guestUpdate.rsvpCount}`)
     res.sendStatus(204)
 })
 
